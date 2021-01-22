70ccb7b8a488d19a1dacd7d4c7c30a4b130e3307
lcy8047 <lcy8047@gmail.com>
2021-01-22 14:34:02 +0900

    Section3 input 21.01.22

---
+++ Input/AddIntegers.cpp
@@ -0,0 +1,40 @@
+#include "AddIntegers.h"
+#include <iostream>
+
+using namespace std;
+
+namespace samples
+{
+	void AddIntegersExample()
+	{
+		cout << "+------------------------------+" << endl;
+		cout << "|     Add Integers Example     |" << endl;
+		cout << "+------------------------------+" << endl;
+	
+
+		int number;
+		int sum = 0;
+
+		while(true)
+		{
+			cout << "Please enter an integer or EOF: ";
+			cin >> number;
+			if (cin.eof())
+			{
+				break;
+			}
+	
+			if(cin.fall())
+			{
+				cout << "Invalid input" << endl;
+				cin.clear();
+				cin.ignore(LLONG_MAX, '\n');
+				continue;
+			}
+			sum += number;
+		}
+		cin.clear();
+	
+		cout << "The sum is " << sum << endl;
+	}
+}
---
+++ Input/ReverseInputString.cpp
@@ -0,0 +1,39 @@
+#include "ReverseInputString.h"
+#include <iostream>
+
+using namespace std;
+
+namespace samples
+{
+	void ReverseInputStringExample()
+	{
+		cout << "+------------------------------+" << endl;
+		cout << "|    Reverse String Example    |" << endl;
+		cout << "+------------------------------+" << endl;
+
+		const int LINE_SIZE = 512;
+		char line[LINE_SIZE];
+
+		cout << "Please enter a string to reverse" << endl
+			<< "or EOF to quit: ";
+		cin.getline(line. LINE_SIZE);
+		if (cin.fail())
+		{
+			cin.clear();
+			return;
+		}
+
+		char* p = line;
+		char* q = line + strlen(line) - 1;
+		while(p < q)
+		{
+			char temp = *p;
+			*p = *q;
+			*q = temp;
+			
+			++p;
+			--q;
+		}
+		cout << "Reversed string: " << line << endl;
+	}
+}
