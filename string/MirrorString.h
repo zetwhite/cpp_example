81e631ff2d85c21dfd150671ca3ad977eaba79dd
lcy8047 <lcy8047@gmail.com>
2021-01-25 16:44:10 +0900

    Add MirrorString 2021.01.25

---
+++ string/MirrorString.h
@@ -0,0 +1,4 @@
+namespace samples
+{
+	void MirrorStringExample();
+}
---
+++ string/MirrorTestMain.cpp
@@ -0,0 +1,9 @@
+//g++ -o main MirrorTestMain.cpp Mirror.cpp -I.
+
+#include "MirrorString.h"
+
+int main(void)
+{
+	samples::MirrorStringExample();
+	return 0;
+}
