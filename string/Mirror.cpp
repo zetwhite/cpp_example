066c96d7f029b26e5fbca3a93da5ff84f7eae877
lcy8047 <lcy8047@gmail.com>
2021-01-25 16:37:38 +0900

    Add Mirror.cpp 2021.01.25

---
+++ string/Mirror.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include "MirrorString.h"
+
+using namespace std;
+
+namespace samples
+{
+	void MirrorStringExample()
+	{
+		cout << "+------------------------------+" << endl;
+		cout << "|    Mirror String Example     |" << endl;
+		cout << "+------------------------------+" << endl;
+
+		string line = "Hello World!";
+
+		cout << "string to mirror: " << line << endl;
+
+		for(int i = (int)line.size() - 1; i >= 0; --i)
+		{
+			line += line[i];
+		}
+		cout << "mirrored string" << line << endl;
+	}
+}
