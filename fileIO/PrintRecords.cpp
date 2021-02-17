5bc4b2f58c779e1c6e3f625e17e24fef98f790cf
lcy8047 <lcy8047@gmail.com>
2021-02-17 23:53:35 +0900

    fileIO example

---
+++ fileIO/PrintRecords.cpp
@@ -0,0 +1,117 @@
+#include <fstream>
+#include <iomanip>
+#include "PrintRecords.h"
+
+using namespace std;
+
+namespace samples
+{
+	Record ReadRecord(istream& stream, bool bPrompt)
+	{
+		Record record;
+
+		if(bPrompt)
+		{
+			cout << "First name: ";
+		}
+		stream >> record.FirstName;
+
+		if(bPrompt)
+		{
+			cout << "Last name: ";
+		}
+		stream >> record.LastName;
+
+		if(bPrompt)
+		{
+			cout << "Student ID: ";
+		}
+		stream >> record.StudentID;
+
+		if(bPrompt)
+		{
+			cout << "Score: ";
+		}
+		stream >> record.Score;
+
+		return record
+	}
+
+	void DisplayRecords(fstream& fileStream)
+	{
+		fileStream.seekg(0);
+
+		string line;
+		while(true)
+		{
+			getline(fileStream, line);
+
+			if(fileStream.eof())
+			{
+				fileStream.clear();
+				break;
+			}
+			cout << line << endl;
+		}
+	}
+
+	void WriteFileRecord(fstream& outputStream, const Record& record)
+	{
+		outputStream.seekp(0, ios_base::end);
+
+		outputStream << record.FirstName << " "
+			<< record.LastName << " "
+			<< record.StudentID << " "
+			<< record.Score << endl;
+
+		outputStream.flush();
+	}
+
+	void ManageRecordExample()
+	{
+		cout << "+------------------------------+" << endl;
+		cout << "|    Manage Records Example    |" << endl;
+		cout << "+------------------------------+" << endl;
+
+		fstream fileStream;
+		fileStream.open("StudentRecords.dat",ios_base::in | ios_base::out);
+
+		bool bExit = false;
+		while(!bExit)
+		{
+			char command = ' ';
+			cout << "a: add" << endl;
+			cout << "d: display" << endl;
+			cout << "x: exit" << endl;
+
+			cin >> command;
+			cin.ignore(LLONG_MAX, '\n');
+
+			switch(command)
+			{
+			case 'a':
+			{
+				Record record = ReadRecord(cin, true);
+				WriteFileRecord(fileStream, record);
+				break;
+			}
+			case 'd':
+			{
+				DisplayRecords(fileStream);
+				break;
+			}
+			case 'x':
+			{
+				bExit = true;
+				break;
+			}
+			default:
+			{
+				cout << "invalid input" << endl;
+				break;
+			}
+			}
+		}
+		fileStream.close();
+	}
+}
---
+++ fileIO/PrintRecords.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+struct Record
+{
+	std::string FirstName;
+	std::string LastName;
+	std::string StudentID;
+	std::string Score;
+};
+
+namespace samples
+{
+	Record ReadRecord(std::istream& stream, bool bPrompt);
+	
+	void WriteFileRecord(std::fstream& outputStream, const Record& record);
+
+	void DisplayRecord(std::fstream& fileStream);
+
+	void ManageRecordExample();
+}
